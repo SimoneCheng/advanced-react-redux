9a9d012b84b5663a5ebec83b8810c57313194552
SimoneCheng <bleach1827@gmail.com>
2022-06-24 22:26:57 +0800

    practice test

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { shallow } from "enzyme";
+
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+})
+
+it("show a comment box", () => {
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("show a comment list", () => {
+    expect(wrapped.find(CommentList).length).toEqual(1);
+});
