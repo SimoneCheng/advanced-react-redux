6942aeab8d77d9a947f9e756b6dbd92dbcb0ef6e
SimoneCheng <bleach1827@gmail.com>
2022-06-25 21:39:24 +0800

    practice test

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () =>{ 
-    return(
-        <div>Comment Box</div>
-    );
+class CommentBox extends Component {
+    state = { comment: '' };
+
+    handleChange = (event) => {
+        this.setState({ comment: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        // TODO - call an action creator
+        // and save the comment
+
+        this.setState({ comment: "" });
+    }
+
+    render() {
+        return (
+            <form onSubmit={(e) => this.handleSubmit(e)}>
+                <h4>Add a Comment</h4>
+                <textarea onChange={(e) => this.handleChange(e)} value={this.state.comment} />
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        )
+    }
 }
+
+export default CommentBox;
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { mount } from 'enzyme';
+import CommentBox from "../CommentBox";
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+})
+
+afterEach(() => {
+    wrapped.unmount();
+})
+
+it('has a textarea and a button', () => {
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+});
