5cfab96f95c268d9047f8260fc1819289e47f021
SimoneCheng <bleach1827@gmail.com>
2022-06-28 16:00:06 +0800

    write tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,10 @@ it('has a textarea and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+    wrapped.update();
+});
