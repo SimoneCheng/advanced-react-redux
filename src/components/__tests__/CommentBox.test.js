77d0275ba3b21fb681222489904bacda2cc9ee77
SimoneCheng <bleach1827@gmail.com>
2022-07-10 22:05:46 +0800

    write tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -22,4 +22,16 @@ it('has a text area that users can type in', () => {
         target: { value: 'new comment' }
     });
     wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+it('when form is submitted, textarea get emptied', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+    wrapped.update();
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,4 @@
-import Enzyme from 'Enzyme';
+import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
+enzyme.configure({ adapter: new Adapter() });
